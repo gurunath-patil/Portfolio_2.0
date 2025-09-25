148737b07d01e84a62338741535a9446bfa13650
gurunath-patil <gurunathpatil8099@gmail.com>
2025-09-26 00:19:29 +0530

    Fixed app height layout issue

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/navBar'
 import { Outlet } from 'react-router-dom'
 function App() {
 	return (
-		<div className='bg-black flex flex-col justify-center p-4 md:p-4 gap-y-3 md:gap-y-5 lg:flex-row lg:gap-x-5'>
+		<div className='bg-black flex flex-col justify-center p-4 md:p-4 gap-y-3 md:gap-y-5 lg:flex-row lg:gap-x-5 h-dvh'>
 			<SideMenuConatiner />
 			<div className='bg-[#0b192c] rounded-lg overflow-hidden w-full relative'>
 				<NavBar />
